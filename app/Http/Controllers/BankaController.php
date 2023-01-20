81816fae481eb28862683faec3c4ad79ab62f01d
JovanaMartic <jovanamartic1@gmail.com>
2023-01-20 22:18:10 +0100

    postavljene api rute i kontroleri

---
+++ app/Http/Controllers/BankaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Banka;
+use Illuminate\Http\Request;
+
+class BankaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Banka  $banka
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Banka $banka)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Banka  $banka
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Banka $banka)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Banka  $banka
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Banka $banka)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Banka  $banka
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Banka $banka)
+    {
+        //
+    }
+}
